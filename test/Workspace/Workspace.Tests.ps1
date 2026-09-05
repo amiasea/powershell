8833d23d4cf5c3ac09249899fed2dbe4f6f6aff2
Alfredo Ball <alfredo.ball@outlook.com>
2026-09-05 15:29:54 -0500

    build test - 2

--- test/Workspace/Workspace.Tests.ps1
+++ test/Workspace/Workspace.Tests.ps1
@@ -1,51 +1,59 @@
 BeforeAll {
-    $modulePath = Join-Path $PSScriptRoot '../../src/Workspace/Workspace.psd1'
+    $modulePath = Join-Path `
+        $PSScriptRoot `
+        '../../src/Workspace/Workspace.psd1'
 
     Import-Module $modulePath -Force
 }
 
 Describe 'Workspace' {
     BeforeEach {
-        $workspaceRoot = Join-Path $TestDrive 'workspaces'
-
         $repositories = @(
-            [WorkspaceRepository]@{
-                repository = 'example/api'
-                name       = 'api'
-                clone_url  = 'https://github.com/example/api.git'
+            [pscustomobject]@{
+                Name     = 'api'
+                CloneUrl = 'https://github.com/example/api.git'
             }
 
-            [WorkspaceRepository]@{
-                repository = 'example/web'
-                name       = 'web'
-                clone_url  = 'https://github.com/example/web.git'
+            [pscustomobject]@{
+                Name     = 'web'
+                CloneUrl = 'https://github.com/example/web.git'
             }
 
-            [WorkspaceRepository]@{
-                repository = 'example/platform-api'
-                name       = 'platform-api'
-                clone_url  = 'https://github.com/example/platform-api.git'
+            [pscustomobject]@{
+                Name     = 'platform-api'
+                CloneUrl = 'https://github.com/example/platform-api.git'
             }
 
-            [WorkspaceRepository]@{
-                repository = 'example/platform-web'
-                name       = 'platform-web'
-                clone_url  = 'https://github.com/example/platform-web.git'
+            [pscustomobject]@{
+                Name     = 'platform-web'
+                CloneUrl = 'https://github.com/example/platform-web.git'
             }
         )
 
-        $workspace = Resolve-Workspace -Repositories $repositories
+        Mock `
+            -CommandName Get-GitHubRepository `
+            -ModuleName Workspace `
+            -MockWith {
+                $repositories
+            }
     }
 
     It 'resolves repository data into a Workspace' {
+        $workspace = Resolve-Workspace -Organization 'example'
+
         $workspace | Should -BeOfType ([Workspace])
         $workspace.root | Should -Be '/workspaces'
-        $workspace.children | Should -HaveCount 4
+        $workspace.children | Should -HaveCount 3
     }
 
     It 'resolves standalone repositories as repository nodes' {
+        $workspace = Resolve-Workspace -Organization 'example'
+
         $node = $workspace.children |
-            Where-Object { $_.type -eq 'repository' -and $_.name -eq 'api' }
+            Where-Object {
+                $_.type -eq 'repository' -and
+                $_.name -eq 'api'
+            }
 
         $node | Should -Not -BeNullOrEmpty
         $node | Should -BeOfType ([WorkspaceRepository])
@@ -56,8 +64,13 @@ Describe 'Workspace' {
     }
 
     It 'resolves shared repository prefixes as logical parents' {
+        $workspace = Resolve-Workspace -Organization 'example'
+
         $node = $workspace.children |
-            Where-Object { $_.type -eq 'logical_parent' -and $_.name -eq 'platform' }
+            Where-Object {
+                $_.type -eq 'logical_parent' -and
+                $_.name -eq 'platform'
+            }
 
         $node | Should -Not -BeNullOrEmpty
         $node | Should -BeOfType ([WorkspaceLogicalParent])
@@ -66,8 +79,13 @@ Describe 'Workspace' {
     }
 
     It 'resolves logical parent repository paths' {
+        $workspace = Resolve-Workspace -Organization 'example'
+
         $node = $workspace.children |
-            Where-Object { $_.type -eq 'logical_parent' -and $_.name -eq 'platform' }
+            Where-Object {
+                $_.type -eq 'logical_parent' -and
+                $_.name -eq 'platform'
+            }
 
         $api = $node.children |
             Where-Object { $_.name -eq 'api' }
@@ -79,11 +97,44 @@ Describe 'Workspace' {
         $web.path | Should -Be 'platform/web'
     }
 
+    It 'queries GitHub using the specified organization' {
+        Resolve-Workspace -Organization 'example' | Out-Null
+
+        Should -Invoke `
+            -CommandName Get-GitHubRepository `
+            -ModuleName Workspace `
+            -Times 1 `
+            -Exactly `
+            -ParameterFilter {
+                $OrganizationName -eq 'example'
+            }
+    }
+
     It 'builds the resolved Workspace' {
+        $workspace = Resolve-Workspace -Organization 'example'
+
+        $workspaceRoot = Join-Path $TestDrive 'workspaces'
+
+        $workspace.root = $workspaceRoot
+
+        Mock `
+            -CommandName git `
+            -ModuleName Workspace `
+            -MockWith {
+                $global:LASTEXITCODE = 0
+            }
+
         Build-Workspace -Workspace $workspace
 
-        $workspacePath = Join-Path $workspaceRoot ''
-        
-        $workspacePath | Should -Exist
+        $workspaceRoot | Should -Exist
+
+        Should -Invoke `
+            -CommandName git `
+            -ModuleName Workspace `
+            -Times 3 `
+            -Exactly `
+            -ParameterFilter {
+                $args[0] -eq 'clone'
+            }
     }
 }
