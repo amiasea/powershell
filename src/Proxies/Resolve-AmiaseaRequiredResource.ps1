01e22be3997590d39c840bb8681580b4c4a26595
Alfredo Ball <alfredo.ball@outlook.com>
2026-09-07 05:49:57 -0500

    Update Proxies Tests

--- src/Proxies/Resolve-AmiaseaRequiredResource.ps1
+++ src/Proxies/Resolve-AmiaseaRequiredResource.ps1
@@ -8,14 +8,24 @@ function Resolve-AmiaseaRequiredResource {
         [object]$Version
     )
 
+    Write-Host '=== RESOLVE AMIASEA RESOURCE ==='
+    Write-Host "Name: [$Name]"
+    Write-Host "Version supplied: [$($null -ne $Version)]"
+    Write-Host "Version: [$Version]"
+
     $findParameters = @{
         Name        = $Name
-        Repository = 'Amiasea'
+        Repository  = 'Amiasea'
         ErrorAction = 'Stop'
     }
 
     if ($null -ne $Version) {
         $findParameters['Version'] = $Version
+    }
+
+    Write-Host 'Find-PSResource parameters:'
+    foreach ($key in $findParameters.Keys) {
+        Write-Host "  $key = [$($findParameters[$key])]"
     }
 
     $resource = Find-PSResource @findParameters |
@@ -25,11 +35,43 @@ function Resolve-AmiaseaRequiredResource {
         throw "Amiasea resource '$Name' could not be found."
     }
 
+    Write-Host 'Resolved resource:'
+    Write-Host "  Name: [$($resource.Name)]"
+    Write-Host "  Version: [$($resource.Version)]"
+    Write-Host "  Prerelease: [$($resource.Prerelease)]"
+    Write-Host "  Repository: [$($resource.Repository)]"
+
+    if ([string]$resource.Name -ne $Name) {
+        throw "Resolver invariant violated: requested '$Name', but Find-PSResource returned '$($resource.Name)'."
+    }
+
+    if ($null -ne $Version) {
+        if ([string]$resource.Version -ne [string]$Version) {
+            throw "Resolver invariant violated: requested version '$Version', but Find-PSResource returned '$($resource.Version)'."
+        }
+    }
+
     $requiredResource = @{}
 
-    foreach ($dependency in @($resource.Dependencies)) {
+    $dependencies = @($resource.Dependencies)
+
+    Write-Host "Dependency count: $($dependencies.Count)"
+
+    foreach ($dependency in $dependencies) {
+        Write-Host '--- DEPENDENCY ---'
+        Write-Host "Name: [$($dependency.Name)]"
+        Write-Host "VersionRange: [$($dependency.VersionRange)]"
+        Write-Host "VersionRange type: [$($dependency.VersionRange.GetType().FullName)]"
+        Write-Host "Repository: [$($dependency.Repository)]"
+        Write-Host "Dependency type: [$($dependency.GetType().FullName)]"
+
         if (-not $dependency.Name) {
+            Write-Host 'Skipping dependency with no name.'
             continue
+        }
+
+        if (-not $dependency.VersionRange) {
+            throw "Resolver invariant violated: dependency '$($dependency.Name)' has no VersionRange."
         }
 
         $dependencySpec = @{
@@ -40,8 +82,34 @@ function Resolve-AmiaseaRequiredResource {
             $dependencySpec['repository'] = [string]$dependency.Repository
         }
 
+        Write-Host 'RequiredResource entry:'
+        foreach ($key in $dependencySpec.Keys) {
+            Write-Host "  $key = [$($dependencySpec[$key])]"
+        }
+
         $requiredResource[[string]$dependency.Name] = $dependencySpec
+
+        Write-Host '--- END DEPENDENCY ---'
     }
+
+    Write-Host '=== RESOLVER OUTPUT ==='
+
+    if ($requiredResource.Count -eq 0) {
+        Write-Host 'RequiredResource is empty.'
+    }
+    else {
+        foreach ($dependencyName in $requiredResource.Keys) {
+            $dependencySpec = $requiredResource[$dependencyName]
+
+            Write-Host "  $dependencyName"
+
+            foreach ($key in $dependencySpec.Keys) {
+                Write-Host "    $key = [$($dependencySpec[$key])]"
+            }
+        }
+    }
+
+    Write-Host '=== END RESOLVER ==='
 
     return ,$requiredResource
 }
